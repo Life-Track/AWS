ddb9eb7f5ecd1882d7065cc93acd5baf1003b61a
Connor Robinson <connorrobinson121@gmail.com>
2025-06-23 20:18:31 -0400

    bootstrapping state setup

---
+++ bootstrap_backend.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-state-connor"
+
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
