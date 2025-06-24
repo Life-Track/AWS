7baee0b88f94c97fefa5b6a9fd20b54f3f855dce
Connor Robinson <connorrobinson121@gmail.com>
2025-06-23 20:31:08 -0400

    testing some add/destroy by adding

--- main.tf
+++ main.tf
@@ -2,15 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_s3_bucket" "example" {
-#   bucket = "example-bucket-from-terraform-${random_id.suffix.hex}"
-#   force_destroy = true
+resource "aws_s3_bucket" "example" {
+  bucket = "this-is-connors-test"
+  force_destroy = true
 
-#   tags = {
-#     Name = "Example"
-#   }
-# }
-
-resource "random_id" "suffix" {
-  byte_length = 4
+  tags = {
+    Name = "Example"
+  }
 }
