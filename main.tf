a9c0cddeb844433a2b09ff2a019fc33280f3c4a4
Connor Robinson <connorrobinson121@gmail.com>
2025-06-23 20:32:41 -0400

    testing add/destroy by destroying

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "this-is-connors-test"
-  force_destroy = true
+# resource "aws_s3_bucket" "example" {
+#   bucket = "this-is-connors-test"
+#   force_destroy = true
 
-  tags = {
-    Name = "Example"
-  }
-}
+#   tags = {
+#     Name = "Example"
+#   }
+# }
