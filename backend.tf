0e4379a645e523b2d78fbd8847de6bd3b4172ba0
Connor Robinson <connorrobinson121@gmail.com>
2025-06-23 20:23:41 -0400

    adding backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "my-terraform-state-bucket"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-life-track"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
