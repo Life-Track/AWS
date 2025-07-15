e87a72f74c7500efc4a2cbdd533f965943df88f9
Connor Robinson <connorrobinson121@gmail.com>
2025-07-15 17:29:56 -0400

    removing test bucket

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-# resource "aws_s3_bucket" "example" {
-#   bucket = "this-is-connors-test"
-#   force_destroy = true
-
-#   tags = {
-#     Name = "Example"
-#   }
-# }
