4d52e81d86a24227cd425989a259def095a50415
Connor Robinson <connorrobinson121@gmail.com>
2025-07-16 21:18:55 -0400

    Testing Module Integration

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+locals {
+  finance_module_version = "v0.1.1"
+}
+
+module "finance" {
+  source = "https://github.com/Life-Track/finance.git?ref=${locals.finance_module_version}"
+
+  s3_name = "beans_bang_wife"
+}
