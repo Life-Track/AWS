541f5c5e7a75ccda542aa687466b53993fb52c3c
Connor Robinson <connorrobinson121@gmail.com>
2025-07-16 21:29:53 -0400

    More Formatting for integration

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ provider "aws" {
 
 locals {
   finance_module_version = "v0.1.1"
+  finance_module_repo    = "https://github.com/Life-Track/finance.git"
+  finance_module_source  = "${finance_module_repo}?ref=${finance_module_version}"
 }
 
 module "finance" {
-  source = format("https://github.com/Life-Track/finance.git?ref=%s", locals.finance_module_version)
+  source  = locals.finance_module_source
   s3_name = "beans_bang_wife"
 }
