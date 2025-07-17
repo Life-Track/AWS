4320aaf2a5e06ce94339e499c34cb8204bfff2a7
Connor Robinson <connorrobinson121@gmail.com>
2025-07-16 21:42:19 -0400

    bumping finance for integration

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 locals {
-  finance_module_version = "v0.1.2"
+  finance_module_version = "v0.1.3"
   finance_module_repo    = "https://github.com/Life-Track/finance.git"
   finance_module_source  = "${locals.finance_module_repo}?ref=${locals.finance_module_version}"
 }
 
 module "finance" {
-  source  = "git::https://github.com/Life-Track/finance.git?ref=v0.1.2"
+  source  = "git::https://github.com/Life-Track/finance.git?ref=v0.1.3"
   s3_name = "beans_bang_wife"
 }
