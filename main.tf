ccdf1b342213d4c8d8a581b96ae8db7fa618a3b9
Connor Robinson <connorrobinson121@gmail.com>
2025-07-16 21:26:40 -0400

    reformatting source module

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ locals {
 }
 
 module "finance" {
-  source = "https://github.com/Life-Track/finance.git?ref=${locals.finance_module_version}"
-
+  source = format("https://github.com/Life-Track/finance.git?ref=%s", locals.finance_module_version)
   s3_name = "beans_bang_wife"
 }
